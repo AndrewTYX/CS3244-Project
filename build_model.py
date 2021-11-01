24cd1f6d3d7551bac72bf5edbe0d87582266432b
Geyu <russellgy1229@gmail.com>
2021-11-01 13:53:58 +0800

    combine model

--- build_model.py
+++ build_model.py
@@ -1,37 +1,83 @@
-def build_input_layer(ct_input_shape, raw_input_shape):
+from tensorflow.keras.layers import LSTM, Input, Conv3D, MaxPool3D, BatchNormalization, Flatten, Dense, Dropout, Concatenate
+from tensorflow.keras import Model
+from tensorflow.keras.models import Sequential
+
+def build_input_layer(ct_input_shape, raw_input_shape, base_input_shape):
     ct_input = Input(ct_input_shape)
     time_input = Input(raw_input_shape)
-    return ct_input, time_input
+    base_input = Input(base_input_shape)
+    return ct_input, time_input, base_input
     
-def build_cnn_layers(input_layer, feature_size):
-    conv_layer1 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(input_layer)
-    pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer1)
-    pooling_layer1 = BatchNormalization()(pooling_layer1)  
-    conv_layer2 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer1)
-    pooling_layer2 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer2)
-    pooling_layer2 = BatchNormalization()(pooling_layer2)
-    conv_layer3 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer1)
-    pooling_layer3 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer3)
-    pooling_layer3 = BatchNormalization()(pooling_layer3)
-    conv_layer4 = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer3)
-    pooling_layer4 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer4)
-    pooling_layer4 = BatchNormalization()(pooling_layer4)
-    conv_layer5 = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer4)
-    pooling_layer5 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer5)
-    conv_layer6 = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer5)
-    pooling_layer6 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer6)
-    conv_layer7 = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer6)
-    pooling_layer7 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer7)
+def build_cnn_layers(input_shape, feature_size):
+    model = Sequential()
+    model.add(Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same', input_shape=input_shape))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu', padding='same'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu', padding='same'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
+    model.add(Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu', padding='same'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
+    model.add(Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu', padding='same'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='same'))
     
-    pooling_layer9 = BatchNormalization()(pooling_layer7)
-    flatten_layer = Flatten()(pooling_layer9)
+    model.add(BatchNormalization())
+    model.add(Flatten())
     
-    dense_layer3 = Dense(units=512, activation='relu')(flatten_layer)
-    dense_layer3 = Dropout(0.4)(dense_layer3)
+    model.add(Dense(units=512, activation='relu'))
+    model.add(Dropout(0.4))
 
-    dense_layer4 = Dense(units=256, activation='relu')(dense_layer3)
-    dense_layer4 = Dropout(0.4)(dense_layer3)
+    model.add(Dense(units=256, activation='relu'))
+    model.add(Dropout(0.4))
   
-    feature_layer = Dense(units=feature_size, activation='relu')(dense_layer4)
+    model.add(Dense(units=feature_size, activation='relu'))
     
-    return feature_layer
+    return model
+
+def build_ff_layers(input_shape, feature_size):
+    model = Sequencial()
+    model.add(Dense(units=16, activation='relu', input_shape = input_shape))
+    model.add(Dense(units=32, activation='relu'))
+    model.add(Dense(units=feature_size, activation='softmax'))
+    return model
+
+def build_LSTM(nn_feature_size, ff_feature_size, time_feature_size):
+    n_features = nn_feature_size + ff_feature_size + time_feature_size
+    n_steps = 2
+    
+    model = Sequential()
+    model.add(LSTM(50, activation='relu', return_sequences = True, input_shape=(n_steps, n_features)))
+    model.add(LSTM(50, activation = 'relu'))
+    model.add(Dense(1))
+    
+    return model
+    
+def build_full_lstm(ct_input_shape, raw_input_shape, base_input_shape,
+                    nn_feature_size, ff_feature_size):
+    ct_input, time_input, base_input = build_input_layer(ct_input_shape, raw_input_shape, base_input_shape)
+    
+    x1 = build_cnn_layers(ct_input_shape, nn_feature_size)(ct_input)
+    x2 = time_input
+    x3 = build_ff_layers(base_input_shape, ff_feature_size)(base_input)
+    
+    out = Concatenate()([x1, x2, x3])
+    
+    out = build_LSTM(nn_feature_size, ff_feature_size, 2)
+    
+    model = Model([ct_input, time_input, base_input], out)
+    
+    return model
+    
+    
+    
+    
+    
+    
