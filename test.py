d7ab64a8645887e6eca18371aaa692e2edffb1e1
Geyu <russellgy1229@gmail.com>
2021-11-01 14:32:56 +0800

    fix model

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from build_model import build_cnn_layers
 from build_model import build_ff_layers
 from build_model import build_LSTM
 from tensorflow.keras.layers import LSTM, Input, Conv3D, MaxPool3D, BatchNormalization, Flatten, Dense, Dropout, Concatenate
+from tensorflow.keras import Model
+
 ct_input_shape = (2,512, 512, 3, 1)
 raw_input_shape = (2, 2)
 base_input_shape = (2,4)
@@ -23,3 +25,6 @@ print(x3)
 out = Concatenate()([x1, x2, x3])
     
 out = build_LSTM(nn_feature_size, ff_feature_size, 2)(out)
+
+model = Model([ct_input, time_input, base_input], out)
+model.summary()
