a3010cd0b3961ead14ad6a9a323d471df1e8ee03
Geyu <russellgy1229@gmail.com>
2021-11-01 15:25:03 +0800

    change the preprocessing pipe

--- data_processing.py
+++ data_processing.py
@@ -4,9 +4,43 @@ import os
 import random
 import tensorflow as tf
 from generate_ct_data import save_np_arr_with_channel
+from sklearn.preprocessing import LabelEncoder 
 
 csv_path = './train.csv'
 BAD_IDS = {'ID00011637202177653955184', 'ID00052637202186188008618'}
+
+def interpolate_FVC(patient_df):
+    time = np.array(dataset['Weeks'])
+    value = np.array(dataset['FVC'])
+    flinear = interpolate.interp1d(time, value, kind = 'slinear')
+    #fqua = interpolate.interp1d(time, value, kind = 'quadratic')
+
+    uniformWeek = np.arange(time[0], time[-1] + 1)
+    valueLinear = flinear(uniformWeek).astype('int64')
+
+    df = pd.DataFrame({'Weeks': uniformWeek, 'FVC': valueLinear})
+    return df
+
+def construct_timeseries_input(patient_data, features, steps):
+  input_seqs = []
+  for feature in features:
+    seq = data[feature]
+    seq = np.array(seq)
+    seq = seq.reshape(len(seq), 1)
+    input_seqs.append(seq)
+
+  #horizontally stack the input columns
+  dataset = np.hstack(input_seqs)
+  
+  #split a multivariate sequence into samples
+  X, y = list(), list()
+  for i in range(len(dataset)):
+    end_ix = i + steps
+    if end_ix >= len(dataset): break
+    seq_x, seq_y = dataset[i:end_ix, :-1], dataset[end_ix, -1]  
+    X.append(seq_x)
+    y.append(seq_y)
+  return np.array(X), np.array(y)
 
 def get_ids(df_path):
     df = pd.read_csv(df_path)
@@ -28,122 +62,70 @@ def split_ids(id_list, train_ratio, test_ratio, val_ratio):
         
     return train_ids, test_ids, val_ids ;
 
-def build_time_series_df(df_path, patient_ids):
-    '''
-    Build ds for time series data
-    '''
-    print('[Data Preprocessing] Building time series dataset...')
-    dataset = pd.read_csv(df_path)
-    patients_dataset = dataset.loc[dataset['Patient'].isin(patient_ids)]
-    # Output a dataframe
-    return patients_dataset[['Patient', 'Weeks', 'FVC']]
+def get_ct_for_patient(ct_dir, patient_id):
+    return ct_dir[patient_id]
 
-def build_time_series_ds(df_path, patient_ids):
-    df = build_time_series_df(df_path, patient_ids)
-    length = len(df)
-    arr = np.empty((length, 2))
-    index = 0
-    for patient in patient_ids:
-        patient_df = df[df['Patient'] == patient]
-        temp_arr = patient_df[['Weeks', 'FVC']].to_numpy()
+def duplicate_with_timestep(arr, step):
+    # Using hstack to duplicate
+    res = np.copy(arr)
+    
+    for i in range(0, step):
+        res = np.hstack((res, arr))
         
-        for i in range(0, len(temp_arr)):
-            arr[index] = temp_arr[i]
-            index = index + 1
-    return tf.data.Dataset.from_tensor_slices(arr)
-
-def generate_ID_count_map(time_series_df, patient_ids):
-    '''
-    Genereate ID -> data instance count map for time series map
-    For building corresponding 
-    
-    Return a list for order consistent with patient ids
-    res[0] is the count for patient_ids[0]
-    '''
-    res = []
-    value_count_frame = time_series_df.Patient.value_counts()
-    for patient in patient_ids:
-        res += value_count_frame[patient]
-    
     return res
 
-# def build_baseline_df(df_path, patient_ids):
-#     '''
-#     Build ds for baseline data
-#     '''
-#     dataset = pd.read_csv(df_path)
-#     print('[Data Preprocessing] Building baseline dataset...')
-#     patients_dataset = dataset.loc[dataset['Patient'].isin(patient_ids)]
-#     # Output a dataframe
-#     return patients_dataset[['Patient', 'Age', 'Sex', 'SmokingStatus']]
-
-def build_baseline_ds(time_series_df, patient_ids):
-    sex_mapping = {'Male': 0, 'Female': 1}
-    smoking_mapping = {'Ex-smoker':0, 'Never smoked':1, 'Currently smokes':2}
-    id_count = generate_ID_count_map(time_series_df, patient_ids)
-    length = len(time_series_df)
-    arr = np.empty((length, 3))
-    index = 0
-    for i in range(0, len(patient_ids)):
-        repeat = id_count[i]
-        curr_id = patient_ids[i]
-        curr_df = time_series_df[time_series_df['Patient'] == curr_id]
-        temp_arr = curr_df.loc[0][['Age', 'Sex', 'SmokingStatus']].to_numpy()
-        temp_arr = [temp_arr[0], sex_mapping[temp_arr[1]], smoking_mapping[temp_arr[2]]]
-        
-        for j in range(0, repeat):
-            arr[index] = temp_arr
-            index = index + 1
-    return tf.data.Dataset.from_tensor_slices(arr)
+def get_baseline_for_patient(patient_df):
+    labelencoder= LabelEncoder()
+    data_encoded = patient_df
+    data_encoded['Sex'] = labelencoder.fit_transform(data_encoded['Sex']) 
+    data_encoded['SmokingStatus'] = labelencoder.fit_transform(data_encoded['SmokingStatus']) 
+    return data_encoded[['Age', 'Sex', 'SmokingStatus']].loc[0].to_numpy()
     
     
-def build_ct_ds(timeseries_df, ct_dir_path, patient_ids, channel_num):
-    print('[Data Preprocessing] Building ct scan dataset...')
-    input_path = f'./ct_interpolated_{channel_num}_dir.npy'
-    assert os.path.exists(input_path)
+def create_seq(data, interp, steps, ct_dir):
+  patient_ID = data['Patient'].unique()
+  time_series_in = np.array([])
+  baseline_in = np.array([])
+  ct_in = np.array([])
+  y_input = np.array([])
+  for ID in patient_ID:
+    patient = data.loc[data['Patient'] == ID]
+    if interp: patient = interpolate_FVC(patient)
+    p_x, p_y = construct_input(patient, ['FVC', 'Weeks', 'FVC'], steps)
+    ct_x = duplicate_with_timestep(get_ct_for_patient(ct_dir, ID), steps)
+    base_x = duplicate_with_timestep(get_baseline_for_patient(patient))
     
-    length = len(time_series_df)
-    ct_dir = np.load(input_path)
-    ct_shape = ct_dir.values()[0].shape
-    id_count = generate_ID_count_map(time_series_df, patient_ids)
-    arr = np.empty((length, ct_shape[0], ct_shape[1], ct_shape[2]))
-    index = 0
-    
-    for i in range(0, len(patient_ids)):
-        repeat = id_count[i]
-        curr_id = patient_ids[i]
-        
-        for i in range(0, repeat):
-            arr[index] = ct_dir[curr_id]
-            index = index + 1
-            
-    return tf.data.Dataset.from_tensor_slices(arr)
-
-def build_label_ds(time_series_df):
-    label = time_series_df['FVC'].to_numpy()
-    return tf.data.Dataset.from_tensor_slices(label)
+    if X_input.size == 0:
+      time_series_in = p_x
+      y_input = p_y
+      ct_in = ct_x
+      baseline_in = base_x
+    else:
+      time_series_in = np.concatenate((time_series_in, p_x))
+      y_input = np.concatenate((y_input, p_y))
+      ct_in = np.concatenate((ct_in, ct_x))
+      baseline_in = np.concatenate((baseline_in, base_x))
+  return tf.data.Dataset.from_tensor_slices(time_series_in),tf.data.Dataset.from_tensor_slices(ct_in), tf.data.Dataset.from_tensor_slices(baseline_in), tf.data.Dataset.from_tensor_slices(y_input)
     
     
-def build_ds(df_path, patient_ids, channel_num):
-    time_series_df = build_time_series_df(df_path, patient_ids)
+def build_ds(df_path, ct_dir_path, patient_ids, timestep):
+    #select data to fit the set
+    data = dataset.loc[dataset['Patient'].isin(patient_ids)]
+    ct_dir = np.load(ct_dir_path)
     
-    timeseries_ds = build_time_series_ds(df_path, patient_ids)
-    baseline_ds = build_baseline_ds(time_series_df, patient_ids)
-    ct_ds = build_ct_ds(time_series_df, ct_dir_path, patient_ids, channel_num)
-    
-    label = build_label_ds(time_series_df)
+    timeseries_ds, baseline_ds, ct_ds, label = create_seq(data, True, time_step, ct_dir)
     return tf.data.Dataset.zip((timeseries_ds, baseline_ds, ct_ds), label)
 
 
-def build_ds_with_split(csv_file_path, ct_dir_path, channel_num):
+def build_ds_with_split(csv_file_path, ct_dir_path, timestep):
     '''
     Top level call for building the dataset
     '''
     all_ids = get_ids(csv_file_path)
     train_ids, test_ids, val_ids = split_ids(all_ids, 0.8, 0.1, 0.1)
-    train_ds = build_ds(csv_file_path, train_ids, channel_num)
-    test_ds = build_ds(csv_file_path, test_ids, channel_num)
-    val_ds = build_ds(csv_file_path, val_ids, channel_num)
+    train_ds = build_ds(csv_file_path, ct_dir_path, train_ids,  timestep)
+    test_ds = build_ds(csv_file_path, ct_dir_path, test_ids,  timestep)
+    val_ds = build_ds(csv_file_path, ct_dir_path, val_ids,  timestep)
     
     return train_ds, test_ds, val_ds
     
--- train_complete.py
+++ train_complete.py
@@ -13,45 +13,15 @@ from tensorflow.keras import Model
 
 # Call back import
 from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from build_model import build_full_lstm
 
-def build_input_layer(ct_input_shape, raw_input_shape):
-    ct_input = Input(ct_input_shape)
-    time_input = Input(raw_input_shape)
-    return ct_input, time_input
-    
-def build_cnn_layers(input_layer, feature_size):
-    conv_layer1 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(input_layer)
-    pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer1)
-    pooling_layer1 = BatchNormalization()(pooling_layer1)  
-    conv_layer2 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer1)
-    pooling_layer2 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer2)
-    pooling_layer2 = BatchNormalization()(pooling_layer2)
-    conv_layer3 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer1)
-    pooling_layer3 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer3)
-    pooling_layer3 = BatchNormalization()(pooling_layer3)
-    conv_layer4 = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer3)
-    pooling_layer4 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer4)
-    pooling_layer4 = BatchNormalization()(pooling_layer4)
-    conv_layer5 = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer4)
-    pooling_layer5 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer5)
-    conv_layer6 = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer5)
-    pooling_layer6 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer6)
-    conv_layer7 = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu', padding='same')(pooling_layer6)
-    pooling_layer7 = MaxPool3D(pool_size=(2, 2, 2), padding='same')(conv_layer7)
-    
-    pooling_layer9 = BatchNormalization()(pooling_layer7)
-    flatten_layer = Flatten()(pooling_layer9)
-    
-    dense_layer3 = Dense(units=512, activation='relu')(flatten_layer)
-    dense_layer3 = Dropout(0.4)(dense_layer3)
+timestep = 2
+ct_input_shape = (timestep,512, 512, 3, 1)
+raw_input_shape = (timestep, 2)
+base_input_shape = (timestep,4)
 
-    dense_layer4 = Dense(units=256, activation='relu')(dense_layer3)
-    dense_layer4 = Dropout(0.4)(dense_layer3)
-  
-    feature_layer = Dense(units=feature_size, activation='relu')(dense_layer4)
-    
-    return feature_layer
-
+nn_feature_size = 10
+ff_feature_size = 10
 # Build ds for time series and ct scan
 
 # Zip the ds with labels
